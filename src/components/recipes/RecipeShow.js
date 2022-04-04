74b2d26b3646a935b84d34fe63927fea166063f8
jfw2855 <jwaddle1130@gmail.com>
2022-04-04 12:31:16 -0500

    adds recipeshow route

--- src/components/recipes/RecipeShow.js
+++ src/components/recipes/RecipeShow.js
@@ -1,14 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import { Card } from 'react-bootstrap'
-import { Link,useLocation } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import { showRecipe } from '../../api/recipes'
 
 
 
 
-const RecipeIndex = (props) => {
+const RecipeShow = (props) => {
+
+    const [recipe,setRecipe] = useState(null)
+    const { user,msgAlert} = props
+    const {id} = useParams()
+
+    useEffect(() => {
+        showRecipe(id)
+            .then(res => {
+                setRecipe(res.data)
+                console.log('apiresponse',res.data)
+
+            })
+            .then(() =>
+                msgAlert({
+                    heading: 'Success!',
+                    message: `recipe show page`,
+                    variant: 'success',
+                }))
+            .catch(()=> {
+                msgAlert({
+                    heading: 'Oh No!',
+                    message: 'Issue with showing recipe',
+                    variant: 'danger',
+                })})
+    }, [])
 
 
-    return 
+
+    if (!recipe) {
+        return <p>loading...</p>
+    } 
+
+
+    return (
+        <>
+            <h3>{recipe.title}</h3>
+            <div >
+
+                
+            </div>
+        </>
+    )
 }
 
-export default RecipeIndex
+export default RecipeShow
