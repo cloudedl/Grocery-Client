cded8e2a48c67023caf0bd30b1ce1a2a16f11a6a
jfw2855 <jwaddle1130@gmail.com>
2022-04-03 21:49:20 -0500

    initial commit

---
+++ src/components/recipes/RecipeIndex.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import { Card } from 'react-bootstrap'
+import { Link,useLocation } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
+import { searchRecipe } from '../../api/recipes'
+
+
+
+const RecipeIndex = (props) => {
+
+
+    return 
+}
+
+export default RecipeIndex
+
+
