98cef3c1e5df2e3e9bd1698201f6ba8379ff1734
jfw2855 <jwaddle1130@gmail.com>
2022-04-03 21:40:57 -0500

    initial commit

---
+++ src/components/recipes/RecipeSearch.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { Form, Container, Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+
+
+
+
+const RecipeSearch = (props) => {
+
+
+
+    return 
+
+}
+
+
+
+
+
+
+
+export default RecipeSearch
+
