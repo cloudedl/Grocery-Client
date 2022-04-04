6d5837d18957b7a7646fa0beaa0fd7986761b82b
jfw2855 <jwaddle1130@gmail.com>
2022-04-03 21:35:03 -0500

    adds search recipe function for external api

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -3,8 +3,18 @@ import axios from 'axios'
 
 
 
-// Search Function -> GETs results from recipe query
+// Search Recipe Function -> GETs results from recipe query
 
+export const searchRecipe = (query) => {
+    const searchUrl = 'https://api.spoonacular.com/recipes/complexSearch?query='
+    const apiKey = 'INPUT API KEY HERE'
+
+    const config = {
+        method: 'get',
+        url: `${searchUrl+query}&addRecipeInformation=true&number=10&apiKey=${apiKey}`
+    }
+    return axios(config)
+}
 
 // Show Function -> GETs recipe information 
 
