ce893686fa735ea007fa72c55ae83d4e7e83ec28
jfw2855 <jwaddle1130@gmail.com>
2022-04-04 10:08:58 -0500

    adds showRecipe function that gets recipe info from external api

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 
 
 
-// Search Recipe Function -> GETs results from recipe query
+// Search Recipe Function -> GETs query results from external api
 
 export const searchRecipe = (query) => {
     const searchUrl = 'https://api.spoonacular.com/recipes/complexSearch?query='
-    const apiKey = 'INPUT API KEY HERE'
+    const apiKey = 'ENTER API KEY HERE'
 
     const config = {
         method: 'get',
@@ -16,8 +16,21 @@ export const searchRecipe = (query) => {
     return axios(config)
 }
 
-// Show Function -> GETs recipe information 
+// Show Function -> GETs recipe information from external api
 
-// Price Function -> GETs recipe price breakdown for each ingredient
+// spoonId === spoonacular's id for the recipe
+
+export const showRecipe = (spoonId) => {
+    const apiKey = 'ENTER API KEY HERE'
+    const searchUrl = `https://api.spoonacular.com/recipes/${spoonId}/information?includeNutrition=false&apiKey=${apiKey}`
+
+    const config = {
+        method: 'get',
+        url: `${searchUrl}`
+    }
+    return axios(config)
+}
+
+// Price Function -> GETs recipe price breakdown for each ingredient from external api
 
 
