8d1ba7d2d57947b0a4ded2db0efe17c29d2b2ea5
cloud3999 <edwardlcloud@gmail.com>
2022-04-05 09:24:18 -0400

    added working name to home and header

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -66,7 +66,7 @@ const Header = ({ user }) => (
 	<Navbar bg='primary' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
-                react-auth-template
+                NutriCart
             </Link>
         </Navbar.Brand>
 		<Navbar.Toggle aria-controls='basic-navbar-nav' />
