08b1291ca85ddb04317fcb52da5d33a6fd3b1af1
jfw2855 <jwaddle1130@gmail.com>
2022-04-06 14:10:36 -0500

    adds individual items and all items to cart

--- src/components/recipes/RecipeShow.js
+++ src/components/recipes/RecipeShow.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { showRecipe,ingPrice } from '../../api/recipes'
+import {addItem} from '../../api/cart'
 import { Form, Container, Button, Card, Link, Row, Col } from 'react-bootstrap'
+
 
 
 
@@ -13,6 +15,7 @@ const RecipeShow = (props) => {
     const [recipe,setRecipe] = useState(null)
     // ings === ingredients
     const [ings,setIngs] = useState(null)
+    const [ingArray,setIngArray] = useState([])
     const { user,msgAlert} = props
     const {id} = useParams()
     console.log("recipe", recipe, "this is the total cost")
@@ -39,6 +42,7 @@ const RecipeShow = (props) => {
         ingPrice(id)
             .then(res => {
                 setIngs(res.data)
+                setIngArray (res.data.ingredients)
                 console.log('apiresponse',res.data)
 
             })
@@ -50,21 +54,83 @@ const RecipeShow = (props) => {
                 })})
     }, [])
 
+    
+    
 
 
     if (!recipe) {
         return <p>loading...</p>
     } 
-    const handleSubmit = (e) => {
+    const handleAddIng = (e,index) => {
         //e === event
         e.preventDefault()
-        //current navigate just to test handleSubmit
-        navigate('/recipe')
+
+
+            let ingObj = {
+                "name":  ingArray[index].name,
+                "price": ingArray[index].price
+            }
+        addItem(user,ingObj)
+            .then()
+            .then(() =>
+            msgAlert({
+                heading: 'Success!',
+                message: 'item added',
+                variant: 'success',
+            }))
+            // if there is an error, we'll send an error message
+            .catch(() =>
+                msgAlert({
+                    heading: 'Oh No!',
+                    message: 'issue with add item',
+                    variant: 'danger',
+                }))
+
+
+
     }
+
+    const handleAddAll = (e) => {
+        //e === event
+        e.preventDefault()
+
+      let ingFixedArr = []
+
+
+
+        for (let i = 0; i<ingArray.length;i++) {
+            let ingObj = {
+                "name":  ingArray[i].name,
+                "price": ingArray[i].price
+            }
+            ingFixedArr.push(ingObj)
+        }
+
+    const addItemArr = () => {
+        ingFixedArr.map( ingredient=> {
+            addItem(user,ingredient)
+            .then()
+        // if there is an error, we'll send an error message
+        .catch(() =>
+            msgAlert({
+                heading: 'Oh No!',
+                message: 'issue with add item',
+                variant: 'danger',
+            }))
+        }) 
+    }
+
+    addItemArr()
+
+}
+
+
+    
    
-    let ingArray = ings.ingredients
-    console.log('this is ingArray', ingArray)
-    const recipeCard = ingArray.map( ingredient=> ( 
+   
+    
+
+    const recipeCard = ingArray.map( (ingredient,index) => ( 
 
         <Card 
             bg={"light"}
@@ -83,7 +149,7 @@ const RecipeShow = (props) => {
                     <Card.Text>
                     
                     <div className="d-grid gap-2">
-                    <Form onSubmit={handleSubmit}>
+                    <Form onClick={(e)=>handleAddIng(e,index)}>
                      <Button fluid ="true" type='submit'>Add Item to Cart</Button>
                      </Form>
                        
@@ -112,7 +178,7 @@ const RecipeShow = (props) => {
           
             <Container className='justify-content-center'>
                 
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleAddAll}>
             <Button type='submit'>Add Recipe to Cart</Button>
             </Form>
         </Container>
@@ -121,3 +187,206 @@ const RecipeShow = (props) => {
 }
 
 export default RecipeShow
+
+
+
+
+
+// FROM CONSOLE LOG INGARRAY 
+// const ingArray = [
+//     {
+//         "name": "all purpose flour",
+//         "image": "flour.png",
+//         "price": 16.67,
+//         "amount": {
+//             "metric": {
+//                 "value": 125,
+//                 "unit": "g"
+//             },
+//             "us": {
+//                 "value": 1,
+//                 "unit": "cup"
+//             }
+//         }
+//     },
+//     {
+//         "name": "canned beer",
+//         "image": "beer.jpg",
+//         "price": 183.7,
+//         "amount": {
+//             "metric": {
+//                 "value": 340.194,
+//                 "unit": "g"
+//             },
+//             "us": {
+//                 "value": 12,
+//                 "unit": "oz"
+//             }
+//         }
+//     },
+//     {
+//         "name": "diced bell pepper",
+//         "image": "bell-pepper-orange.png",
+//         "price": 59.76,
+//         "amount": {
+//             "metric": {
+//                 "value": 1,
+//                 "unit": ""
+//             },
+//             "us": {
+//                 "value": 1,
+//                 "unit": ""
+//             }
+//         }
+//     },
+//     {
+//         "name": "cajun seasoning",
+//         "image": "chili-powder.jpg",
+//         "price": 87.75,
+//         "amount": {
+//             "metric": {
+//                 "value": 27,
+//                 "unit": "g"
+//             },
+//             "us": {
+//                 "value": 0.25,
+//                 "unit": "cup"
+//             }
+//         }
+//     },
+//     {
+//         "name": "diced canned tomatoes",
+//         "image": "tomatoes-canned.png",
+//         "price": 60.75,
+//         "amount": {
+//             "metric": {
+//                 "value": 283.495,
+//                 "unit": "g"
+//             },
+//             "us": {
+//                 "value": 10,
+//                 "unit": "oz"
+//             }
+//         }
+//     },
+//     {
+//         "name": "diced canned tomatoes",
+//         "image": "tomatoes-canned.png",
+//         "price": 88.09,
+//         "amount": {
+//             "metric": {
+//                 "value": 411.068,
+//                 "unit": "g"
+//             },
+//             "us": {
+//                 "value": 14.5,
+//                 "unit": "oz"
+//             }
+//         }
+//     },
+//     {
+//         "name": "diced celery",
+//         "image": "celery.jpg",
+//         "price": 90.13,
+//         "amount": {
+//             "metric": {
+//                 "value": 6,
+//                 "unit": "stalks"
+//             },
+//             "us": {
+//                 "value": 6,
+//                 "unit": "stalks"
+//             }
+//         }
+//     },
+//     {
+//         "name": "canned chicken broth",
+//         "image": "chicken-broth.png",
+//         "price": 396.39,
+//         "amount": {
+//             "metric": {
+//                 "value": 1.233,
+//                 "unit": "kilogram"
+//             },
+//             "us": {
+//                 "value": 43.5,
+//                 "unit": "oz"
+//             }
+//         }
+//     },
+//     {
+//         "name": "cooked chicken breasts",
+//         "image": "cooked-chicken-breast.png",
+//         "price": 1076.72,
+//         "amount": {
+//             "metric": {
+//                 "value": 3,
+//                 "unit": ""
+//             },
+//             "us": {
+//                 "value": 3,
+//                 "unit": ""
+//             }
+//         }
+//     },
+//     {
+//         "name": "extra virgin olive oil",
+//         "image": "olive-oil.jpg",
+//         "price": 257.31,
+//         "amount": {
+//             "metric": {
+//                 "value": 216,
+//                 "unit": "ml"
+//             },
+//             "us": {
+//                 "value": 1,
+//                 "unit": "cup"
+//             }
+//         }
+//     },
+//     {
+//         "name": "garlic powder",
+//         "image": "garlic-powder.png",
+//         "price": 29.45,
+//         "amount": {
+//             "metric": {
+//                 "value": 1,
+//                 "unit": "Tbsp"
+//             },
+//             "us": {
+//                 "value": 1,
+//                 "unit": "Tbsp"
+//             }
+//         }
+//     },
+//     {
+//         "name": "parsley",
+//         "image": "parsley.jpg",
+//         "price": 30.1,
+//         "amount": {
+//             "metric": {
+//                 "value": 2,
+//                 "unit": "Tbsps"
+//             },
+//             "us": {
+//                 "value": 2,
+//                 "unit": "Tbsps"
+//             }
+//         }
+//     },
+//     {
+//         "name": "diced sweet onion",
+//         "image": "sweet-onion.png",
+//         "price": 94.89,
+//         "amount": {
+//             "metric": {
+//                 "value": 1,
+//                 "unit": ""
+//             },
+//             "us": {
+//                 "value": 1,
+//                 "unit": ""
+//             }
+//         }
+//     }
+// ]
