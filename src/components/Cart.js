86b8e99c582421a742ca26d4dc0331dcfc651e59
jfw2855 <jwaddle1130@gmail.com>
2022-04-06 23:32:10 -0500

    displays rows of items from cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,45 +1,127 @@
 import React, { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import {viewCart} from '../api/cart'
+import { Form, Container, Button, Card, Link, Row, Col, ListGroup} from 'react-bootstrap'
+import { AiFillDelete,AiFillPlusSquare,AiFillMinusSquare } from "react-icons/ai";
 
 export default function Cart(props) {
-    // example array
-    const cartItemsArray = [
-        {
-            name: 'tomato',
-            price: 1.00
-        },
-        {
-            name: 'banana',
-            price: 0.50
-        },
-        {
-            name: 'potato',
-            price: 2.00
-        }
-    ]
 
-    const cartItemsDisplay = cartItemsArray.map(item => {
-        return (
-            <h4>{item.name} {item.price}</h4>
-        )
-    })
+    const styleObj = {
+        fontSize: 20,
+        color: "#4a54f1",
+        textAlign: "center",
+        paddingTop: "100px",
+    }
+    const navigate = useNavigate()
+    const [cart,setCart] = useState(null)
+    const [itemsArr,setItemsArr] = useState([])
+    const { user,msgAlert} = props
+    console.log('this is user',user._id)
+    useEffect(() => {
+        viewCart(user)
+            .then(res => {
+                setCart(res.data.cart.items)
+                
+            })
+            .then(()=> {
+                console.log('this is cart',cart)
+            })
+            .then(() =>
+                msgAlert({
+                    heading: 'Success!',
+                    message: `recipe show page`,
+                    variant: 'success',
+                }))
+            .catch(()=> {
+                msgAlert({
+                    heading: 'Oh No!',
+                    message: 'Issue with showing recipe',
+                    variant: 'danger',
+                })})
+    }, [])
 
-    const cartTotal = cartItemsArray.reduce((a, b) => {
-        return (
-            {price: a.price + b.price}
-        )
-    })
+    if (!cart) {
+        return <p>loading...</p>
+    } 
+
+
+
+    let itemsDisplay
+
+
+    if(cart.length>0) {
+        itemsDisplay = cart.map( (item,index) => ( 
+            <ListGroup.Item key={item._id}>
+                <Row>
+                    <Col className="item-info">
+                        <span>{item.name}</span>
+                    </Col>
+                    <Col className="item-info">
+                        <span>${(item.qty*item.price/100).toFixed(2)}</span>
+                    </Col>
+                    <Col className="item-info">
+                        <span>Quantity: {item.qty}</span>
+                    </Col>
+
+                    <Col className="item-info">
+                        <Button
+                            type="button"
+                            variant="success"
+                        >
+                            <AiFillPlusSquare fontSize="18px" />
+                        </Button>
+                        
+                    </Col>
+
+                    <Col className="item-info">
+                        <Button
+                            type="button"
+                            variant="warning"
+                        >
+                            <AiFillMinusSquare fontSize="18px" />
+                        </Button>
+                        
+                    </Col>
+                    <Col className="item-info">
+                        <Button
+                            type="button"
+                            variant="danger"
+                        >
+                            <AiFillDelete fontSize="18px" />
+                        </Button>
+                        
+                    </Col>
+                </Row>
+            </ListGroup.Item>
+            
+
+            
+    
+        ))
+
+
+    }
+
+
+
+
+
+
+
+
 
     return (
-        <>
-            <div>
-                <header>
-                    <h2>Cart</h2>
-                </header>
-                <div>
-                    {cartItemsDisplay}
-                    {cartTotal.price}
-                </div>
+        <div className='cart-showpage'>
+            <div className='items-container'>
+                <ListGroup className="cart-list">
+                    {itemsDisplay}
+                </ListGroup>
             </div>
-        </>
+            <div className='cart-summary'>
+                <span className='subtotal'>Subtotal {cart.length} items</span>
+                <span style={{fontWeight: 700, fontSize:20}}>Total: </span>
+                <Button type="button" disabled={cart.length===0}>Proceed to Checkout</Button>
+            </div>
+        </div>
     )
 }
