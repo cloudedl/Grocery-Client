564140f3388ad0a5025e62b1befa818d85093211
johnzwiss <johnzwiss@gmail.com>
2022-04-07 09:52:57 -0400

    padding and margins added

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -59,6 +59,7 @@ const SignIn = (props) => {
     <Container
     style={{ 
         marginTop: "10%",
+        padding: "10px",
         width: '50%', 
         boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
         fontFamily: "Times New Roman" }} >
@@ -88,7 +89,7 @@ const SignIn = (props) => {
                             onChange={e => setPassword(e.target.value)}
                         />
                     </Form.Group>
-                    <Button  variant='primary' type='submit'>
+                    <Button style ={{marginTop: "2%"}} variant='primary' type='submit'>
                         Submit
                     </Button>
                 </Form>
